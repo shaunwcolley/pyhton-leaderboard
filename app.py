29ca8c1b6a2edb31a52e146415e3b8bc093fe7df
Shaun Colley <swc7838@gmail.com>
2019-10-02 11:19:52 -0500

    parsing json data of scores sent to server, need to work on api-key generation and middleware checks for api key, build out database

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Server Works!'
 
-@app.route('/greet')
-def say_hello():
-    return 'Hello from Server'
-
-@app.route('/user/<username>')
-def show_user(username):
-    return 'Username: %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'post: ' + str(post_id)
+@app.route('/log_score', methods=['GET','POST'])
+def log_score():
+    if request.method == 'POST':
+        info = request.get_json()
+        print(info['initials'])
+        print(info['score'])
+        return 'post'
+    elif request.method == 'GET':
+        return 'get'
